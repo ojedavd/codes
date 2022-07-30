12ee8d05079695d231ecb2546c78ab504b2a5cf5
ojedavd <ojedavd@gmail.com>
2022-07-29 22:39:31 -0300

    flexbox

---
+++ html/app.js
@@ -0,0 +1,5 @@
+var img = document.createElement("img");
+img.src = "https://miracomosehace.com/wp-content/uploads/2020/06/logo-google-chrome-5.jpg";
+
+var src = document.getElementById('body-id');
+src.appendChild(img);
